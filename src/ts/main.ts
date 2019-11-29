0e537f6c080e59897141b6b0de05b883ab0e15bf
nvarner <nathanmvarner@protonmail.com>
2019-11-29 17:49:05 -0500

    Change to sidebar model

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -8,6 +8,8 @@ import "../../node_modules/leaflet/dist/leaflet.css";
 import "../style.scss";
 import { SidebarController } from "../Sidebar/SidebarController";
 import LRoomLabel from "../LRoomLabelPlugin/LRoomLabelPlugin";
+import "../../node_modules/leaflet-sidebar-v2/css/leaflet-sidebar.min.css";
+import "leaflet-sidebar-v2";
 
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("/serviceWorker.js");
@@ -30,18 +32,30 @@ const leafletMap = L.map("map", {
 
 leafletMap.fitBounds(bounds.pad(0.05));
 
+const sidebar = L.control.sidebar({
+    container: "sidebar",
+    closeButton: true
+}).addTo(leafletMap);
+
+sidebar.addPanel({
+    "id": "search",
+    "tab": "<i class=\"fas fa-search-location\"></i>",
+    "title": "Search",
+    "pane": "<input type=\"text\" id=\"searchbar\"></input>"
+});
+
 // @ts-ignore: JSON works fine here
 const map = new MapData(mapData, bounds);
 
-const attribution = "<a href='https://www.nathanvarner.com'>Nathan Varner</a>";
+const attribution = "<a href='https://www.nathanvarner.com' target='_blank'>Nathan Varner</a>";
 const floors = new LFloors(map, "1", { "attribution": attribution });
 floors.addTo(leafletMap);
 
-const sidebarEl = document.getElementById("sidebar");
-const sidebarController = new SidebarController(sidebarEl, map, floors, leafletMap);
+// const sidebarEl = document.getElementById("sidebar");
+// const sidebarController = new SidebarController(sidebarEl, map, floors, leafletMap);
 
-floors.addLayer(new LRoomLabel(map, "1", sidebarController));
-floors.addLayer(new LRoomLabel(map, "2", sidebarController));
+// floors.addLayer(new LRoomLabel(map, "1", sidebarController));
+// floors.addLayer(new LRoomLabel(map, "2", sidebarController));
 
 // Display dev layer and location of mouse click when dev is enabled
 const devLayer1 = map.createDevLayerGroup("1");
