50097168c1fba33053002a80f13a98bda52e9b2c
nvarner <nathanmvarner@protonmail.com>
2021-05-30 23:42:13 -0400

    Improve LFloors and refactor FloorsControl out

---
+++ src/ts/LFloorsPlugin/FloorsControl.tsx
@@ -0,0 +1,80 @@
+import { fromMap } from "@nvarner/monads";
+import { Control, DomEvent, Util } from "leaflet";
+import { h } from "../JSX";
+import { zip } from "../utils";
+
+/** Control used to choose the current floor to view */
+export class FloorsControl extends Control {
+    private readonly controlElement: HTMLElement;
+    private readonly floorControls: Map<string, HTMLElement>;
+
+    public constructor(
+        floors: string[],
+        defaultFloor: string,
+        setFloor: (floor: string) => void,
+        options?: L.ControlOptions
+    ) {
+        super(options);
+
+        [this.controlElement, this.floorControls] = FloorsControl.createElement(floors, defaultFloor, setFloor);
+    }
+
+    public initialize(options: L.ControlOptions): void {
+        Util.setOptions(this, options);
+    }
+
+    public onAdd(_map: L.Map): HTMLElement {
+        return this.controlElement;
+    }
+
+    /**
+     * Set the styling of the control to reflect a new floor being set
+     */
+    public setFloor(oldFloorNumber: string, newFloorNumber: string): void {
+        fromMap(this.floorControls, oldFloorNumber).ifSome(oldControl => FloorsControl.deselectControl(oldControl));
+        fromMap(this.floorControls, newFloorNumber).ifSome(newControl => FloorsControl.selectControl(newControl));
+    }
+
+    /**
+     * Create the HTML element representing the control
+     * @returns `[controlElement, floorControls]`
+     * `controlElement` is the HTML element representing the control. `floorControls` is a mapping from room number to the
+     * part of `controlElement` that changes when the floor changes.
+     */
+    private static createElement(
+        floors: string[],
+        defaultFloor: string,
+        setFloor: (floor: string) => void
+    ): [HTMLElement, Map<string, HTMLElement>] {
+        const controls = floors.map(floor => <a href="#">{floor}</a> as HTMLElement);
+        zip(floors, controls)
+            .filter(([floor, _control]) => floor === defaultFloor)
+            .map(([_floor, control]) => FloorsControl.selectControl(control));
+
+        const callbacks = zip(floors, controls).map(([floor, control]) => () => {
+            setFloor(floor);
+            controls.forEach(otherControl => FloorsControl.deselectControl(otherControl));
+            FloorsControl.selectControl(control);
+        });
+
+        zip(controls, callbacks).forEach(([control, callback]) => control.addEventListener("click", callback));
+        
+        const floorControls = new Map(zip(floors, controls));
+
+        const base = <div class="leaflet-bar leaflet-control leaflet-control-floors" /> as HTMLElement;
+        controls.forEach(control => base.appendChild(control));
+
+        DomEvent.disableClickPropagation(base);
+        DomEvent.disableScrollPropagation(base);
+
+        return [base, floorControls];
+    }
+
+    private static selectControl(control: HTMLElement): void {
+        control.classList.add("selected");
+    }
+
+    private static deselectControl(control: HTMLElement): void {
+        control.classList.remove("selected");
+    }
+}
--- src/ts/LFloorsPlugin/LFloorsPlugin.ts
+++ src/ts/LFloorsPlugin/LFloorsPlugin.ts
@@ -1,17 +1,21 @@
-import { Some, None, Option, fromMap, Result, Err, Ok } from "@nvarner/monads";
+import { fromMap, Result, Err, Ok } from "@nvarner/monads";
 
 import "./floors.scss";
 import { MapData } from "../MapData";
-import { Control, DomEvent, imageOverlay, Layer, layerGroup, LayerGroup, LayerOptions, Util } from "leaflet";
+import { imageOverlay, Layer, layerGroup, LayerGroup, LayerOptions } from "leaflet";
 import { zip } from "../utils";
+import { FloorsControl } from "./FloorsControl";
 
 export class LFloors extends LayerGroup {
+    /** Mapping from floor numbers to floors */
     private readonly allFloors: Map<string, LayerGroup>;
-    private readonly defaultFloorNumber: string;
 
-    private control: Option<LFloorsControl>;
+    /** Control used to switch between layers on the map */
+    private readonly control: FloorsControl;
 
+    /** Floor currently being rendered */
     private currentFloor: LayerGroup;
+    /** Floor number of `currentFloor` */
     private currentFloorNumber: string;
 
     /** Stores all additions to the map by floor number */
@@ -48,14 +52,13 @@ export class LFloors extends LayerGroup {
         const currentFloor = resCurrentFloor.unwrap();
 
         return Ok(
-            new LFloors(options, allFloors, None, defaultFloorNumber, currentFloor, defaultFloorNumber, new Map())
+            new LFloors(options, allFloors, defaultFloorNumber, currentFloor, defaultFloorNumber, new Map())
         );
     }
 
     private constructor(
         options: LayerOptions,
         allFloors: Map<string, L.LayerGroup>,
-        control: Option<LFloorsControl>,
         defaultFloorNumber: string,
         currentFloor: L.LayerGroup,
         currentFloorNumber: string,
@@ -64,18 +67,31 @@ export class LFloors extends LayerGroup {
         super([], options);
         super.addLayer(currentFloor);
 
+        this.control = new FloorsControl(
+            [...allFloors.keys()],
+            defaultFloorNumber,
+            floor => {
+                this.setFloor(floor);
+            },
+            { position: "bottomleft" }
+        );
+
         this.allFloors = allFloors;
-        this.control = control;
-        this.defaultFloorNumber = defaultFloorNumber;
         this.currentFloor = currentFloor;
         this.currentFloorNumber = currentFloorNumber;
         this.additions = additions;
     }
 
+    /** Get the floor number for all floors */
     public getFloors(): IterableIterator<string> {
         return this.allFloors.keys();
     }
 
+    /**
+     * Make Leaflet start rendering `floor` on the map
+     * @param floor Floor to start rendering
+     * @param floorNumber Number of the floor to render
+     */
     private startDrawingFloor(floor: L.LayerGroup, floorNumber: string) {
         super.addLayer(floor);
         fromMap(this.additions, floorNumber).ifSome(additions => {
@@ -83,6 +99,11 @@ export class LFloors extends LayerGroup {
         });
     }
 
+    /**
+     * Make Leaflet stop rendering `floor` on the map
+     * @param floor Floor to stop rendering
+     * @param floorNumber Number of the floor to stop rendering
+     */
     private stopDrawingFloor(floor: L.LayerGroup, floorNumber: string) {
         fromMap(this.additions, floorNumber).ifSome(additions => {
             additions.forEach(addition => floor.removeLayer(addition));
@@ -90,10 +111,14 @@ export class LFloors extends LayerGroup {
         super.removeLayer(floor);
     }
     
+    /**
+     * Switch the currently rendered floor to `floor`. Has no effect if `floor` is already the currently rendered floor.
+     * @param floor Floor to switch to
+     */
     public setFloor(floor: string): this {
         fromMap(this.allFloors, floor).ifSome(newFloor => {
             if (newFloor !== this.currentFloor) {
-                this.control.ifSome(control => control.setFloor(this.currentFloorNumber, floor));
+                this.control.setFloor(this.currentFloorNumber, floor);
 
                 this.stopDrawingFloor(this.currentFloor, this.currentFloorNumber);
 
@@ -107,6 +132,9 @@ export class LFloors extends LayerGroup {
         return this;
     }
 
+    /**
+     * Get the floor number of the floor currently being rendered
+     */
     public getCurrentFloor(): string {
         return this.currentFloorNumber;
     }
@@ -116,6 +144,12 @@ export class LFloors extends LayerGroup {
         return this.addLayerToFloor(layer, floorNumber);
     }
 
+    /**
+     * Adds `layer` to the layer group on a specific floor. Will only be rendered by Leaflet when the given floor is
+     * being rendered.
+     * @param layer Layer to add
+     * @param floorNumber Floor number to add the floor on
+     */
     public addLayerToFloor(layer: L.Layer, floorNumber: string): this {
         const floorLayers = fromMap(this.additions, floorNumber).unwrapOr(new Set());
         floorLayers.add(layer);
@@ -128,7 +162,7 @@ export class LFloors extends LayerGroup {
         return this;
     }
 
-    removeLayer(layer: LSomeLayerWithFloor): this {
+    public removeLayer(layer: LSomeLayerWithFloor): this {
         const floorNumber = layer.getFloorNumber();
         if (floorNumber === this.currentFloorNumber) {
             this.currentFloor.removeLayer(layer);
@@ -141,97 +175,16 @@ export class LFloors extends LayerGroup {
         return this;
     }
 
-    getDefaultFloor(): string {
-        return this.defaultFloorNumber;
-    }
-
-    onAdd(map: L.Map): this {
+    public onAdd(map: L.Map): this {
         super.onAdd(map);
-        
-        const control = new LFloorsControl(
-            this.getFloors(),
-            this.getDefaultFloor(),
-            (floor) => { this.setFloor(floor) },
-            { position: "bottomleft" }
-        );
-        control.addTo(map);
-
-        this.control = Some(control);
-
+        this.control.addTo(map);
         return this;
     }
 
-    onRemove(map: L.Map): this {
+    public onRemove(map: L.Map): this {
         super.onRemove(map);
-        this.control.ifSome(control => map.removeControl(control));
+        map.removeControl(this.control);
         return this;
-    }
-}
-
-class LFloorsControl extends Control {
-    private floors: IterableIterator<string>;
-    private defaultFloor: string;
-    private setFloorCallback: (floor: string) => void;
-    private floorControls: Map<string, HTMLElement>;
-
-    constructor(floors: IterableIterator<string>, defaultFloor: string, setFloorCallback: (floor: string) => void,
-        options?: L.ControlOptions) {
-        super(options);
-        this.floors = floors;
-        this.defaultFloor = defaultFloor;
-        this.setFloorCallback = setFloorCallback;
-        this.floorControls = new Map();
-    }
-
-    initialize(options: L.ControlOptions): void {
-        Util.setOptions(this, options);
-    }
-
-    onAdd(_map: L.Map): HTMLElement {
-        return this.regenerate();
-    }
-
-    regenerate(): HTMLElement {
-        const base = document.createElement("div");
-        base.classList.add("leaflet-bar");
-        base.classList.add("leaflet-control");
-        base.classList.add("leaflet-control-floors");
-        
-        this.floorControls.clear();
-
-        for (const floor of this.floors) {
-            const a = document.createElement("a");
-            a.setAttribute("href", "#");
-            a.addEventListener("click", () => {
-                this.setFloorCallback(floor);
-                for (const otherFloorA of Array.from(base.children)) {
-                    otherFloorA.classList.remove("selected");
-                }
-                a.classList.add("selected");
-            });
-            if (floor === this.defaultFloor) {
-                a.classList.add("selected");
-            }
-
-            const text = document.createTextNode(floor);
-            
-            a.appendChild(text);
-            base.appendChild(a);
-
-            this.floorControls.set(floor, a);
-        }
-
-        DomEvent.disableClickPropagation(base);
-        DomEvent.disableScrollPropagation(base);
-
-        return base;
-    }
-
-    public setFloor(oldFloorNumber: string, newFloorNumber: string) {
-        fromMap(this.floorControls, oldFloorNumber)
-            .ifSome(oldControl => oldControl.classList.remove("selected"));
-        fromMap(this.floorControls, newFloorNumber)
-            .ifSome(newControl => newControl.classList.add("selected"));
     }
 }
 
