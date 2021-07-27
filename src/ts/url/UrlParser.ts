d81dbb27242d11b60cab056dfd417ee8d32daf04
Samuel B Segal <samuel.b.segal@gmail.com>
2021-07-26 20:32:54 -0400

    Updated style

--- src/ts/url/UrlParser.ts
+++ src/ts/url/UrlParser.ts
@@ -1,4 +1,4 @@
-export function isArgumentatedUrl(url:string): boolean{
+export function isArgumentatedUrl(url: string): boolean {
     //This regex could probably be made more efficient, but it's called once
     const urlRegex = /^.*\/(\d+,){2}\d+/;
     const matches = url.match(urlRegex);
