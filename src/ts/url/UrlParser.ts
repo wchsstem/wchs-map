5bc8b87e6c29be3b0c037f6c395fcd3656bc935d
Samuel B Segal <samuel.b.segal@gmail.com>
2021-07-26 20:06:54 -0400

    Updated stylistic aspects of urlparser

--- src/ts/url/UrlParser.ts
+++ src/ts/url/UrlParser.ts
@@ -1,6 +1,6 @@
 export function isArgumentatedUrl(url:string): boolean{
-	//This regex could probably be made more efficient, but it's called once
-	const urlRegex = /^.*\/(\d+,){2}\d+/;
-	let matches = url.match(urlRegex);
-	return matches!=null;
+    //This regex could probably be made more efficient, but it's called once
+    const urlRegex = /^.*\/(\d+,){2}\d+/;
+    const matches = url.match(urlRegex);
+    return matches != null;
 }
