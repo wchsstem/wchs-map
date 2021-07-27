735cdaf0bcb980bb56a687281425b06577aa0786
Samuel B Segal <samuel.b.segal@gmail.com>
2021-07-26 21:55:59 -0400

    Added in untested parser

--- src/ts/url/UrlParser.ts
+++ src/ts/url/UrlParser.ts
@@ -1,3 +1,5 @@
+import { LatLng } from "leaflet";
+
 import { BuildingLocation } from "../BuildingLocation/BuildingLocation";
 
 //Maybe change the pos to be a constant?
@@ -7,4 +9,22 @@ export function isArgumentatedUrl(url: string): boolean {
     const urlRegex = /^.*\/pos:\((\d+,){2}\d+\)/u;
     const matches = url.match(urlRegex);
     return matches != null;
+}
+
+export function parseUrl(url: string): BuildingLocation {
+    const startInd: number = url.lastIndexOf("pos:(");
+    const clippedString: string = url.substring(startInd);
+
+    const endInd: number = url.lastIndexOf(")");
+    const dataString: string = clippedString.substring(0, endInd);
+
+    const data: string[] = dataString.split(",");
+    const x: number = +data[0];
+    const y: number = +data[1];
+    const floor: string = data[2];
+
+    //While it isn't necessarily suited for this data, I think this class works
+    const latlng: LatLng = new LatLng(x, y);
+    const outBuilding: BuildingLocation = new BuildingLocation(latlng, floor);
+    return outBuilding;
 }
