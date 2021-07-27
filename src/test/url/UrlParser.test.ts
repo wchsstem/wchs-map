0f8f489383d29c1cea780aac62561db15127a7a6
Samuel B Segal <samuel.b.segal@gmail.com>
2021-07-26 21:49:01 -0400

    Added in parenths to regex for easy parsing

--- src/test/url/UrlParser.test.ts
+++ src/test/url/UrlParser.test.ts
@@ -12,25 +12,34 @@ describe("isArgumentatedUrl()", () => {
     });
 
     test("returns url that has arguments but no marker", () => {
-        const isArg: boolean = isArgumentatedUrl("www.example.com/#/10,20,30");
+        const isArg: boolean = isArgumentatedUrl(
+            "www.example.com/#/(10,20,30)",
+        );
+        expect(isArg).toBe<boolean>(false);
+    });
+
+    test("returns url with no end parenth", () => {
+        const isArg: boolean = isArgumentatedUrl("www.example.com/#/(10,20,30");
         expect(isArg).toBe<boolean>(false);
     });
 
     test("returns url that has 2 arguments (too few)", () => {
-        const isArg: boolean = isArgumentatedUrl("www.example.com/#/pos:10,20");
+        const isArg: boolean = isArgumentatedUrl(
+            "www.example.com/#/pos:(10,20)",
+        );
         expect(isArg).toBe<boolean>(false);
     });
 
     test("returns url that has full arguments", () => {
         const isArg: boolean = isArgumentatedUrl(
-            "www.example.com/#/pos:10,20,2",
+            "www.example.com/#/pos:(10,20,2)",
         );
         expect(isArg).toBe<boolean>(true);
     });
 
     test("returns url with text based arguments", () => {
         const isArg: boolean = isArgumentatedUrl(
-            "www.example.com/#/pos:ten,twenty,two",
+            "www.example.com/#/pos:(ten,twenty,two)",
         );
         expect(isArg).toBe<boolean>(false);
     });
