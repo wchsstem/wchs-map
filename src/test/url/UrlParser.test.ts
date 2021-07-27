f5ad863cd0326cbb004fef583eadf007dc9d855f
Samuel B Segal <samuel.b.segal@gmail.com>
2021-07-26 21:41:02 -0400

    Updated parser and tests

--- src/test/url/UrlParser.test.ts
+++ src/test/url/UrlParser.test.ts
@@ -1,24 +1,36 @@
 import { isArgumentatedUrl } from "../../ts/url/UrlParser";
 
 describe("isArgumentatedUrl()", () => {
-    test("returns url that has no arguments", () => {
+    test("returns url that has no arguments or marker", () => {
         const isArg: boolean = isArgumentatedUrl("www.example.com/#/");
         expect(isArg).toBe<boolean>(false);
     });
 
+    test("returns url that has marker but no arguments", () => {
+        const isArg: boolean = isArgumentatedUrl("www.example.com/#/pos:");
+        expect(isArg).toBe<boolean>(false);
+    });
+
+    test("returns url that has arguments but no marker", () => {
+        const isArg: boolean = isArgumentatedUrl("www.example.com/#/10,20,30");
+        expect(isArg).toBe<boolean>(false);
+    });
+
     test("returns url that has 2 arguments (too few)", () => {
-        const isArg: boolean = isArgumentatedUrl("www.example.com/#/10,20");
+        const isArg: boolean = isArgumentatedUrl("www.example.com/#/pos:10,20");
         expect(isArg).toBe<boolean>(false);
     });
 
     test("returns url that has full arguments", () => {
-        const isArg: boolean = isArgumentatedUrl("www.example.com/#/10,20,2");
+        const isArg: boolean = isArgumentatedUrl(
+            "www.example.com/#/pos:10,20,2",
+        );
         expect(isArg).toBe<boolean>(true);
     });
 
     test("returns url with text based arguments", () => {
         const isArg: boolean = isArgumentatedUrl(
-            "www.example.com/#/ten,twenty,two",
+            "www.example.com/#/pos:ten,twenty,two",
         );
         expect(isArg).toBe<boolean>(false);
     });
--- src/ts/url/UrlParser.ts
+++ src/ts/url/UrlParser.ts
@@ -1,7 +1,7 @@
 export function isArgumentatedUrl(url: string): boolean {
     //This regex could probably be made more efficient, but it's called once
     // eslint-disable-next-line security/detect-unsafe-regex
-    const urlRegex = /^.*\/(\d+,){2}\d+/u;
+    const urlRegex = /^.*\/pos:(\d+,){2}\d+/u;
     const matches = url.match(urlRegex);
     return matches != null;
 }
