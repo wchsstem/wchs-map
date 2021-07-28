dfddae4abbfe89d40298291f662320ff6de914b8
nvarner <nathanmvarner@protonmail.com>
2021-07-28 14:43:59 -0400

    Add a few Room unit tests

--- src/test/Room.test.ts
+++ src/test/Room.test.ts
@@ -4,6 +4,7 @@
 import { latLng } from "leaflet";
 
 import { BuildingLocation } from "../ts/BuildingLocation/BuildingLocation";
+import { DefinitionTag } from "../ts/Geocoder/DefinitionTag";
 import Room from "../ts/Room";
 
 describe("Room.getName()", () => {
@@ -18,5 +19,33 @@ describe("Room.getName()", () => {
             [],
         );
         expect(room.getName()).toBe<string>("123");
+    });
+
+    test("returns only name with room number when one is available", () => {
+        const room = new Room(
+            [],
+            "123",
+            ["Cafeteria"],
+            [],
+            new BuildingLocation(latLng(0, 0), "1"),
+            5,
+            [],
+        );
+        expect(room.getName()).toBe<string>("Cafeteria (123)");
+    });
+});
+
+describe("Room.hasTag()", () => {
+    test("returns true when tag is present", () => {
+        const room = new Room(
+            [],
+            "123",
+            [],
+            [],
+            new BuildingLocation(latLng(0, 0), "1"),
+            5,
+            [DefinitionTag.EC],
+        );
+        expect(room.hasTag(DefinitionTag.EC)).toBe<boolean>(true);
     });
 });
