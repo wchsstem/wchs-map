0e3adeb529c5b7134e276b31af53e77aea6b90d5
Nathan Varner <nathanmvarner@protonmail.com>
2021-08-11 14:58:00 +0000

    Apply 4 suggestion(s) to 3 file(s)

--- src/ts/url/UrlParser.ts
+++ src/ts/url/UrlParser.ts
@@ -2,9 +2,8 @@ import { LatLng } from "leaflet";
 
 import { BuildingLocation } from "../BuildingLocation/BuildingLocation";
 
-//Maybe change the pos to be a constant?
 export function isArgumentatedUrl(url: string): boolean {
-    //This regex could probably be made more efficient, but it's called once
+    // This regex could probably be made more efficient, but it's called once
     // eslint-disable-next-line security/detect-unsafe-regex
     const urlRegex = /^.*\/pos:\((\d+,){2}\d+\)/u;
     const matches = url.match(urlRegex);
