40014cc5b8e201ea44f84952e68bb4f8ecd50648
Samuel B Segal <samuel.b.segal@gmail.com>
2021-08-16 18:03:46 -0400

    Optionized UrlParser

--- src/test/url/UrlParser.test.ts
+++ src/test/url/UrlParser.test.ts
@@ -4,62 +4,19 @@
 import { LatLng } from "leaflet";
 
 import { BuildingLocation } from "../../ts/BuildingLocation/BuildingLocation";
-import { isArgumentatedUrl, parseUrl } from "../../ts/url/UrlParser";
+import { parseUrl } from "../../ts/url/UrlParser";
 
-describe("isArgumentatedUrl()", () => {
-    test("returns url that has no arguments or marker", () => {
-        const isArg: boolean = isArgumentatedUrl("www.example.com/#/");
-        expect(isArg).toBe<boolean>(false);
-    });
-
-    test("returns url that has marker but no arguments", () => {
-        const isArg: boolean = isArgumentatedUrl("www.example.com/#/pos:");
-        expect(isArg).toBe<boolean>(false);
-    });
-
-    test("returns url that has arguments but no marker", () => {
-        const isArg: boolean = isArgumentatedUrl(
-            "www.example.com/#/(10,20,30)",
-        );
-        expect(isArg).toBe<boolean>(false);
-    });
-
-    test("returns url with no end parenth", () => {
-        const isArg: boolean = isArgumentatedUrl("www.example.com/#/(10,20,30");
-        expect(isArg).toBe<boolean>(false);
-    });
-
-    test("returns url that has 2 arguments (too few)", () => {
-        const isArg: boolean = isArgumentatedUrl(
-            "www.example.com/#/pos:(10,20)",
-        );
-        expect(isArg).toBe<boolean>(false);
-    });
-
-    test("returns url that has full arguments", () => {
-        const isArg: boolean = isArgumentatedUrl(
-            "www.example.com/#/pos:(10,20,2)",
-        );
-        expect(isArg).toBe<boolean>(true);
-    });
-
-    test("returns url with text based arguments", () => {
-        const isArg: boolean = isArgumentatedUrl(
-            "www.example.com/#/pos:(ten,twenty,two)",
-        );
-        expect(isArg).toBe<boolean>(false);
-    });
-});
-
+//Partial coverage: elaborate on this later
 describe("parseUrl()", () => {
     test("returns basic parsed url", () => {
         const latlng: LatLng = new LatLng(20, 10);
         const trueLoc: BuildingLocation = new BuildingLocation(latlng, "2");
 
-        const parsedLoc: BuildingLocation = parseUrl(
+        const urlOption = parseUrl(
             "www.example.com/#/pos:(10,20,2)nonsense_text",
         );
-
-        expect(parsedLoc).toStrictEqual(trueLoc);
+        urlOption.ifSome((parsedLoc: BuildingLocation) => {
+            expect(parsedLoc).toStrictEqual(trueLoc);
+        });
     });
 });
