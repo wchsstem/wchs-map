a487a7e2b359a940ad9f6813d98b59b0d3e9d93f
Samuel B Segal <samuel.b.segal@gmail.com>
2021-08-17 13:36:56 -0400

    Updated for pin

--- src/ts/url/MapUrlView.ts
+++ src/ts/url/MapUrlView.ts
@@ -1,24 +1,27 @@
 import { Map } from "leaflet";
 
+import { Option } from "@nvarner/monads";
+
 import { BuildingLocation } from "../BuildingLocation/BuildingLocation";
 import { LFloors } from "../LFloorsPlugin/LFloorsPlugin";
-import { isArgumentatedUrl, parseUrl } from "./UrlParser";
+import { NavigationPane } from "../Map/View/Sidebar/NavigationPane/NavigationPane";
+import { parseUrl } from "./UrlParser";
 
 const zoomLevel = 5;
-function setViewToBuildLoc(
+
+export function setMapUrlView(
+    url: string,
     map: Map,
     floors: LFloors,
-    loc: BuildingLocation,
+    navPane: NavigationPane,
 ): void {
-    // Handles floor controls
-    floors.setFloor(loc.getFloor());
-    map.setView(loc.getXY(), zoomLevel);
-}
+    const urlOption: Option<BuildingLocation> = parseUrl(url);
+    urlOption.ifSome((loc: BuildingLocation) => {
+        // Sets the view
+        floors.setFloor(loc.getFloor());
+        map.setView(loc.getXY(), zoomLevel);
 
-export function setMapUrlView(map: Map, floors: LFloors, url: string): void {
-    const isArgumentated: boolean = isArgumentatedUrl(url);
-    if (isArgumentated) {
-        const loc: BuildingLocation = parseUrl(url);
-        setViewToBuildLoc(map, floors, loc);
-    }
+        // Sets the pin
+        navPane.moveFromPin(loc);
+    });
 }
