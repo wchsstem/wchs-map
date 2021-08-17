4b9761ee34b7af106d3a9e0fb10669dd127fbd8c
Samuel B Segal <samuel.b.segal@gmail.com>
2021-08-17 13:54:18 -0400

    Prettified urlparser

--- src/ts/url/UrlParser.ts
+++ src/ts/url/UrlParser.ts
@@ -1,22 +1,21 @@
 import { LatLng } from "leaflet";
 
 import { None, Option, Some } from "@nvarner/monads";
+
 import { BuildingLocation } from "../BuildingLocation/BuildingLocation";
 
-
-
 export function parseUrl(url: string): Option<BuildingLocation> {
-	const urlRegex = /pos:\((\d+),(\d+),(\d+)\)/u;
+    const urlRegex = /pos:\((\d+),(\d+),(\d+)\)/u;
 
-	const matches = url.match(urlRegex);
-	if(!matches || matches.length < 4) return None;
+    const matches = url.match(urlRegex);
+    if (!matches || matches.length < 4) return None;
 
-	const x: number = +matches[1];
+    const x: number = +matches[1];
     const y: number = +matches[2];
     const floor: string = matches[3];
 
     const latlng: LatLng = new LatLng(y, x);
     const outBuilding: BuildingLocation = new BuildingLocation(latlng, floor);
-	
+
     return Some(outBuilding);
 }
